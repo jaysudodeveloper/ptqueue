7d08f818b431d911d503ae0260660d079adb9d34
jaytheking <jacques.jamieson@gmail.com>
2017-07-01 03:28:35 +1200

    code clean up and logging working

--- ptqueue/server.py
+++ ptqueue/server.py
@@ -1,10 +1,11 @@
+"""
+Python task queue server
+"""
 import logging
 import socketserver
 import json
 
-"""
-Python task queues
-"""
+# pylint: disable=C0111
 
 LOGGER = logging.getLogger(__name__)
 
@@ -14,13 +15,9 @@ class JsonCommandHandler(socketserver.BaseRequestHandler):
 
     def handle(self):
         data = str(self.request.recv(1024), 'ascii')
-        print(data)
-        LOGGER.debug("received form client: %s", data)
+        # print(data)
+        LOGGER.info("received form client: %s", data)
 
 class PtQueueServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
     """Threaded version of socketserver TCPServer"""
     pass
-
-
-
-
