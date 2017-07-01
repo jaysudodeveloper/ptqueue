ec66d374627230546c82f8e24da3a37ff6712bac
jaytheking <jacques.jamieson@gmail.com>
2017-07-02 04:28:57 +1200

    implemented marshalling to pass test cases

--- ptqueue/util.py
+++ ptqueue/util.py
@@ -2,16 +2,16 @@
 Utility classes and methods that dont really need to exist in seperate files
 """
 
-from decoder import JSONDecoder
-from encoder import JSONEncoder
-
+import json
+import collections
 # pylint: disable=C0103
-_default_decoder = JSONDecoder(object_hook=None, object_pairs_hook=None)
-_default_encoder = JSONEncoder(
+_default_decoder = json.JSONDecoder(object_hook=None, object_pairs_hook=None)
+_default_encoder = json.JSONEncoder(
     skipkeys=False,
     ensure_ascii=True,
     check_circular=True,
     allow_nan=True,
+    sort_keys=True,
     indent=None,
     separators=None,
     default=None,
@@ -20,37 +20,19 @@ _default_encoder = JSONEncoder(
 # pylint: disable=C0111
 class CommandMarshaller(object):
     """Wrapper util class for json to marshal queue commands from client"""
-    def __init__(self, json_command):
-        self._decoder = _default_decoder
-        self._encoder = _default_encoder
-        self.json_command = json_command
+    def __init__(self, json_command_object):
+        self._json_decoder = _default_decoder
+        self._json_encoder = _default_encoder
+        self.json_command_object = json_command_object
         self._command = ""
         self._queue_name = ""
         self._action = ""
         self._data = dict()
 
     def marshal(self):
-        pass
+        command_string = self._json_encoder.encode(self.json_command_object)
+        return command_string.encode("utf-8")
 
     def unmarshal(self):
-        pass
-
-    @property
-    def command(self):
-        pass
-
-    @property
-    def queue(self):
-        pass
-
-    @property
-    def action(self):
-        pass
-
-    @property
-    def arg_data(self):
-        pass
-
-    @property
-    def kwarg_data(self):
-        pass
+        command_string = self.json_command_object.decode("utf-8")
+        return self._json_decoder.decode(command_string)
